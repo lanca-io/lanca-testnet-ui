91e80a4ef18f22cf1f681b64d54f3b6ea49f24e1
babamovandrej <andrejbabamov@gmail.com>
2025-04-08 12:09:45 +0200

    Change text in get tokens common component

--- src/components/common/GetTokens/GetTokens.tsx
+++ src/components/common/GetTokens/GetTokens.tsx
@@ -8,7 +8,7 @@ export const GetTokens: FC = memo(() => {
 	const { getTestTokens, isLoading } = useFaucet()
 
 	const handleGetTokens = useCallback(() => {
-		getTestTokens(84532)
+		getTestTokens(11155111)
 	}, [getTestTokens])
 
 	return (
@@ -16,7 +16,7 @@ export const GetTokens: FC = memo(() => {
 			<div className="get_tokens_content">
 				<h3 className="get_tokens_title">Let's start</h3>
 				<img src="/Swap/GetTokens.svg" alt="Tokens" />
-				<p className="get_tokens_subtitle">Claim test tokens to start testing</p>
+				<p className="get_tokens_subtitle">Claim tCER0 (test tokens) on ETH sepolia to start testing</p>
 			</div>
 			<Button size="l" onClick={handleGetTokens} isLoading={isLoading} data-testid="get-tokens-button">
 				Get Test Tokens
