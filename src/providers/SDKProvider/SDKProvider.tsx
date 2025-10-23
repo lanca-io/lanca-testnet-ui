c620a02c30779dcb46f97e7d399fa602c295be9c
babamovandrej <andrejbabamov@gmail.com>
2025-10-23 12:03:41 +0200

    Setup sdk to work with new chain configuration

--- src/providers/SDKProvider/SDKProvider.tsx
+++ src/providers/SDKProvider/SDKProvider.tsx
@@ -1,21 +1,37 @@
 import type { FC, PropsWithChildren } from 'react'
-import type { LancaClient as Client } from '@lanca/sdk'
+import type { LancaClient as Client, ILancaClientConfig, IChainWithProvider } from '@lanca/sdk'
 import { createContext } from 'react'
 import { LancaClient } from '@lanca/sdk'
-import { sdkConfiguration } from './ClientConfig'
+import { http } from 'viem'
+import { useChainsStore } from '@/stores/chains/useChainsStore'
+import { convertToViemChains } from '@/utils/chains'
 
 export type SDKContext = {
-	client: Client
+    client: Client
 }
 
-const initialContext: SDKContext = {
-	client: new LancaClient(sdkConfiguration),
-}
-
-export const SDKContext = createContext<SDKContext>(initialContext)
+export const SDKContext = createContext<SDKContext | null>(null)
 
 export const SDKProvider: FC<PropsWithChildren<{}>> = ({ children }) => {
-	const client = new LancaClient(sdkConfiguration)
+    const { chains } = useChainsStore()
+    const availableChains = Object.values(chains)
+    const viemChains = convertToViemChains(availableChains)
 
-	return <SDKContext.Provider value={{ client }}>{children}</SDKContext.Provider>
+    const sdkConfiguration: ILancaClientConfig = {
+        chains: viemChains.reduce(
+            (acc, chain) => {
+                acc[chain.id.toString()] = {
+                    chain,
+                    provider: http(chain.rpcUrls.default.http[0]),
+                } as IChainWithProvider
+                return acc
+            },
+            {} as Record<string, IChainWithProvider>,
+        ),
+        testnet: true,
+    }
+
+    const client = new LancaClient(sdkConfiguration)
+
+    return <SDKContext.Provider value={{ client }}>{children}</SDKContext.Provider>
 }
