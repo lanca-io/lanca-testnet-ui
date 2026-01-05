622bfffc5ab2c64ccfdf88a0c43c7bfd62be4690
babamovandrej <andrejbabamov@gmail.com>
2026-01-05 16:26:47 +0100

    Push develop branch to prod

--- src/configuration/constants.ts
+++ src/configuration/constants.ts
@@ -2,4 +2,4 @@ export const project_id = import.meta.env.VITE_PROJECT_ID ?? ''
 export const domain_url = import.meta.env.VITE_DOMAIN_URL ?? ''
 export const posthog_key = import.meta.env.VITE_POSTHOG_KEY ?? ''
 export const posthog_domain = import.meta.env.VITE_POSTHOG_DOMAIN ?? ''
-export const maintenance_mode = false
+export const maintenance_mode = true
